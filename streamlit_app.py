adcbb54391e5dfc57efcec0d13481f708d9b75da
haifamaulafida <haifamaulafida@gmail.com>
2026-05-22 14:14:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,8 @@ st.title("🎈balon")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.badge("New Balon")
+st.badge("Success", icon=":material/check:", color="green")
+
+st.markdown(
+    ":violet-badge[:material/star: 1 balon] :orange-badge[2 balon] :gray-badge[3 balon]"
